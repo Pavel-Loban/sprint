2409bed9134e0adb5148f8b9675a4b40bc9a1106
Pasha <loban_pasha@mail.ru>
2023-03-12 21:35:04 +0300

    add modal-header logout

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
 
 import Avatar from '../../assets/image/avatar.png';
 import Logo from '../../assets/image/logo.svg';
@@ -15,16 +15,16 @@ import styles from './header.module.scss';
 
 export const Header: React.FC = () => {
 
-
+  const push = useNavigate();
   const boxBurgerRef = React.useRef<HTMLElement>(null);
   const box = React.useRef<HTMLDivElement>(null);
   const dispatch = useAppDispatch();
-  const { menuIsOpen} = useAppSelector((state: RootState) => state.burger);
-  const { user} = useAppSelector((state: RootState) => state.user);
+  const { menuIsOpen } = useAppSelector((state: RootState) => state.burger);
+  const { user } = useAppSelector((state: RootState) => state.user);
 
   const getMenuBurger = () => {
     dispatch(setMenuIsOpen(!menuIsOpen));
-    boxBurgerRef.current?.scrollTo(0,0);
+    boxBurgerRef.current?.scrollTo(0, 0);
   };
 
   const getMenuBurgerSpan = (e: React.MouseEvent<HTMLSpanElement>) => {
@@ -39,37 +39,73 @@ export const Header: React.FC = () => {
     if (menuIsOpen) {
 
       document.body.classList.add('menu_burger_open');
-    }else{
+    } else {
       document.body.classList.remove('menu_burger_open');
     }
 
-    boxBurgerRef.current?.scrollTo(0,0);
-  },[menuIsOpen]);
+    boxBurgerRef.current?.scrollTo(0, 0);
+  }, [menuIsOpen]);
+
+  const [modal, setModal] = React.useState<boolean>(false);
+  const modalRef = React.useRef<HTMLDivElement>(null);
+  const avatarRef = React.useRef<HTMLImageElement>(null);
+
 
   React.useEffect(() => {
     const onClickOutsideBurger = (e: MouseEvent) => {
-    const ee = e;
+      const ee = e;
 
-        if( ee.target !== boxBurgerRef.current && ee.target !== box.current){
-          dispatch(setMenuIsOpen(false));
-        }
+      if (ee.target !== boxBurgerRef.current && ee.target !== box.current) {
+        dispatch(setMenuIsOpen(false));
+      }
 
 
     }
 
-    document.body.addEventListener('click',onClickOutsideBurger)
+    document.body.addEventListener('click', onClickOutsideBurger)
 
     return () => {
-      document.body.removeEventListener('click',onClickOutsideBurger);
+      document.body.removeEventListener('click', onClickOutsideBurger);
     };
 
-  },[dispatch])
+  }, [dispatch])
 
+
+
+  const getModal = () => {
+    setModal(!modal);
+  }
+
+  const logout = () => {
+    localStorage.removeItem('tokenData');
+    localStorage.removeItem('user');
+    push('/auth');
+    setModal(!modal);
+}
+
+React.useEffect(() => {
+
+  const onClickOutsideModal = (e: MouseEvent) => {
+    const ee = e;
+
+    if (ee.target !== modalRef.current && ee.target !== avatarRef.current ) {
+      setModal(false);
+    }
+
+
+  }
+
+  document.body.addEventListener('click', onClickOutsideModal)
+
+  return () => {
+    document.body.removeEventListener('click', onClickOutsideModal);
+  };
+},[modal])
 
 
   return (
 
-    <section  className={styles.wrapper}>
+    <section className={styles.wrapper}>
 
 
 
@@ -81,14 +117,19 @@ export const Header: React.FC = () => {
 
       <section className={styles.title_block}>
 
-        <div data-test-id='button-burger' ref={box} className={menuIsOpen ? `${styles.icon_burger} ${styles.icon_burger_active}`: styles.icon_burger} onClick={ getMenuBurger} role='presentation'><span onClick={(e) => getMenuBurgerSpan(e)} role='presentation'/></div>
+        <div data-test-id='button-burger' ref={box} className={menuIsOpen ? `${styles.icon_burger} ${styles.icon_burger_active}` : styles.icon_burger} onClick={getMenuBurger} role='presentation'><span onClick={(e) => getMenuBurgerSpan(e)} role='presentation' /></div>
         <h1 className={styles.title_text}>Библиотека</h1>
         <header className={styles.title_user}>
           <h3 className={styles.user_name}>Привет, {user?.username}!</h3>
-          <img src={Avatar} alt='avatar' className={styles.user_avatar} />
+          <img ref={avatarRef} src={Avatar} alt='avatar' className={styles.user_avatar} onClick={getModal} role='presentation' />
 
+        {modal &&  <div ref={modalRef} className={styles.wrapper_modal_help_logout} >
+            <ul>
+              <li onClick={getModal} role='presentation'>Помощь</li>
+              <li onClick={logout} role='presentation' >Выход</li>
+            </ul>
+          </div>}
 
-          <ModalHelpAndLogout/>
         </header>
       </section>
     </section>
--- src/components/modal-help-and-logout/modal-help-and-logout.tsx
+++ src/components/modal-help-and-logout/modal-help-and-logout.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 import styles from './modal.module.scss';
 
+
+
 export const ModalHelpAndLogout = () => (
-    <div className={styles.wrapper_modal_help_logout} >
+    <div  className={styles.wrapper_modal_help_logout} >
         <ul>
             <li>Помощь</li>
             <li>Выход</li>
